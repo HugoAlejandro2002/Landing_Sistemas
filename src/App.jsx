54c3d8eec7a5362714c241434c30fffcdb38dfe5
lolex <apazahuaychohugoalejandro@gmail.com>
2023-05-18 12:48:20 -0400

    Error corregido de minusculas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 
 
-
-import viteLogo from '/vite.svg'
 import './App.css'
-import NavBar from './components/navbar'
-import Hero from './components/hero'
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 import About from './components/About'
 import Services from './components/Services'
 import Tours from './components/Tours'
